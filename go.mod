97ab5af3675b8eaefb7cf464beaa2a18db5eb70f
Izaak Dale <izaakdaledev@gmail.com>
2024-08-22 17:09:26 -0700

    create client
    Signed-off-by: Izaak Dale <izaakdaledev@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/izaakdale/kafka-client
 
 go 1.21.1
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "my-cluster-kafka-bootstrap:9092",
+		"group.id":          "test-consumer",
+		"auto.offset.reset": "smallest",
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer consumer.Close()
+
+	err = consumer.SubscribeTopics([]string{"test"}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+Loop:
+	for {
+		ev := consumer.Poll(100)
+		switch e := ev.(type) {
+		case *kafka.Message:
+			log.Printf("%+v\n", string(e.Value))
+		case kafka.Error:
+			fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+			break Loop
+		default:
+			fmt.Printf("Ignored %v\n", e)
+		}
+	}
 }
